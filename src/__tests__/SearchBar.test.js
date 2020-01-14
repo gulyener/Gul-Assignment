3ccd5f38ff19220af57dee6a0b2bc2436d37242b
Gul <gulpembeyener@gmail.com>
2020-01-14 16:47:16 +0100

    add tests

--- src/__tests__/SearchBar.test.js
+++ src/__tests__/SearchBar.test.js
@@ -1,112 +1,149 @@
 import React, { useState } from 'react';
 import { shallow, mount } from 'enzyme';
+import { act } from 'react-dom/test-utils';
 import 'jest-styled-components';
-import SearchBar, { ListContainer, Input, Button, Li } from '../components/SearchBar';
+import SearchBar from '../components/SearchBar';
 
-// Enzyme.configure({ adapter: new Adapter() });
+// enzyme.configure({ adapter: new Adapter() });
 
 describe('SearchBar Component', () => {
   let wrapper;
-  const setState = jest.fn();
-  const useStateSpy = jest.spyOn(React, 'useState');
-  useStateSpy.mockImplementation(init => [init, setState]);
 
   beforeEach(() => {
     wrapper = mount(<SearchBar />);
   });
 
   afterEach(() => {
+    wrapper.unmount();
     jest.clearAllMocks();
   });
 
+  // Match snapshot
+  // it('renders like it should', () => {
+  //   const InputContainer = wrapper.find({ 'data-testid': 'InputComponent' });
+  //   expect(InputContainer).toMatchSnapshot();
+  // });
+
+  // Step 1: User sees an input
   it('renders like it should', () => {
     expect(wrapper.isEmptyRender()).toEqual(false);
   });
 
-  // Match snapshot
-  it('renders like it should', () => {
-    const InputContainer = wrapper.find({ 'data-testid': 'InputComponent' });
-    expect(InputContainer.find('input')).toMatchSnapshot();
-  });
-
+  // Step 2: Mouse hovers over the input. Border color changes
   it('changes border color on hover', () => {
     const container = wrapper.find({ 'data-testid': 'InputComponent' });
-
     expect(container).toHaveStyleRule('border', '1px solid #bfc5cd');
     expect(container).toHaveStyleRule('border', '1px solid #4a4a4a', {
       modifier: ':hover',
     });
   });
 
-  it('render input component', () => {
-    const InputElement = wrapper.find('#floatField');
+  //Step 3: User clicks on Input. focus is on input field, Search Icon is visible, List is open
 
-    expect(InputElement).toHaveStyleRule('margin', '0');
-    // expect({
-    //   InputComponent,
-    //   modifier: '&:hover',
-    // }).toHaveStyleRule('border', 'none');
-  });
+  // it('sets list state to active and label moves up', () => {
+  //   const InputWrapper = wrapper.find({ 'data-testid': 'InputField' });
 
-  it('clears searchfield when button gets clicked', () => {
-    const testState = { searchField: '' };
-    const handleClick = jest.fn();
-    const closeButton = wrapper.find({ 'data-testid': 'clear-button' });
+  //   InputWrapper.find('input').simulate('click');
+  //   expect(InputWrapper.find('input').get(0).ref.current).toEqual(document.activeElement);
 
-    closeButton.simulate('click');
-    expect(handleClick).toHaveBeenCalled();
-    expect(ListComponent.state(active)).toEqual(true);
-  });
+  //   console.log(document.activeElement);
+  //   const InputElement = InputWrapper.at(0);
+  //   expect(InputElement.props().active).toBe(true);
+  // });
 
-  it('changes searchField state when user types', () => {
-    const testState = { searchField: '' };
+  // Step 4: User starts typing, text color changes. List is open, List gets filtered.
+  // OnChange searchfield
+  // it('changes searchField state when user types', () => {
+  //   const testState = { searchField: '' };
 
-    const InputComponent = shallow(
-      <Input
-        value={testState.searchField}
-        onChange={e => {
-          testState.searchField = e.target.value;
-        }}
-      />,
-    );
+  //   const InputComponent = shallow(
+  //     <Input
+  //       value={testState.searchField}
+  //       onChange={e => {
+  //         testState.searchField = e.target.value;
+  //       }}
+  //     />,
+  //   );
 
-    InputComponent.simulate('change', {
-      target: { value: 'John Doe' },
+  //   InputComponent.simulate('change', {
+  //     target: { value: 'John Doe' },
+  //   });
+
+  //   expect(testState.searchField).toEqual('John Doe');
+  // });
+
+  // Close button - on first click search field gets cleared, on second click it closes the list
+  it('closes list when button gets clicked', () => {
+    const InputBefore = wrapper.find({ 'data-testid': 'InputField' }).at(0);
+
+    // Focus on input element so that close button is rendered
+    act(() => {
+      InputBefore.prop('onFocus')();
     });
+    wrapper.update();
 
-    expect(testState.searchField).toEqual('John Doe');
+    // List is open, button is rendered
+    expect(wrapper.find({ 'data-testid': 'DropdownListComponent' }).props().isOpen).toBe(true);
+    const closeButton = wrapper.find({ 'data-testid': 'CloseButton' }).at(0);
+    closeButton.simulate('click');
+    wrapper.update();
+
+    // List is closed
+    expect(wrapper.find({ 'data-testid': 'DropdownListComponent' }).props().isOpen).toBe(false);
   });
 
-  // handleClick
+  // if field is not empty
+  it('clears searchfield when button gets clicked', () => {
+    const InputField = wrapper.find({ 'data-testid': 'InputField' }).at(0);
 
-  it('calls onCLick handler with the right arguments', () => {
-    const testState = { searchField: '' };
-    const handleClick = event => {
-      testState.searchField = event.target.dataset.id;
-    };
-    const mockItem = [{ name: 'Wade Dugmore' }];
-    const wrapperList = mount(
-      <Li data-id={mockItem[0].name} key={mockItem[0].name} onClick={handleClick}>
-        {mockItem[0].name}
-      </Li>,
-    );
+    // Focus on input element and enter a value
+    act(() => {
+      InputField.prop('onChange')({ target: { value: 'John Doe' } });
+      InputField.prop('onFocus')();
+    });
+    wrapper.update();
 
-    wrapperList.simulate('click', { target: wrapperList }).at(0);
-    expect(testState.searchField).toEqual('Jane Doe');
-    expect(wrapperList.clicked).toEqual(true);
-    expect(handleClick.mock.calls.length).toEqual(1);
-    expect(handleClick.calledWith(mockItem, false)).to.be.true;
+    // Check if there is value in search field
+    expect(
+      wrapper
+        .find({ 'data-testid': 'InputField' })
+        .at(0)
+        .props().value,
+    ).toBe('John Doe');
+
+    const closeButton = wrapper.find({ 'data-testid': 'CloseButton' }).at(0);
+    closeButton.simulate('click');
+    wrapper.update();
+
+    // Search field is empty, List is open
+    expect(
+      wrapper
+        .find({ 'data-testid': 'InputField' })
+        .at(0)
+        .props().value,
+    ).toBe('');
+    expect(wrapper.find({ 'data-testid': 'DropdownListComponent' }).props().isOpen).toBe(true);
+
+    // Click for the second time
+    closeButton.simulate('click');
+    wrapper.update();
+
+    // List is closed
+    expect(wrapper.find({ 'data-testid': 'DropdownListComponent' }).props().isOpen).toBe(false);
   });
 
-  // handleFocus
+  // Step 5: Selection is made. Text color is #4a4a4a
+  // Test handleClick function
 
-  it('sets list state to active and label moves up', () => {
-    const InputWrapper = wrapper.find({ 'data-testid': 'InputField' });
-
-    InputWrapper.find('input').simulate('click');
-    expect(InputWrapper.find('input').get(0).ref.current).toEqual(document.activeElement);
-
-    const InputElement = wrapper.find('input');
-    expect(InputElement.prop('placeholder')).toBe(true);
+  it('changes searchField state when user makes selection', () => {
+    const ListItem = wrapper.find('li').at(0);
+    ListItem.simulate('click');
+    wrapper.update();
+    expect(
+      wrapper
+        .find({ 'data-testid': 'InputField' })
+        .at(0)
+        .props().value,
+    ).toBe(ListItem.text());
   });
 });
